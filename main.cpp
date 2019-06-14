48f4401da709fd103d9045b30791eef9114e3e66
Recep Mehmet Akgül <recepmehmetakgul@hotmail.com>
2019-06-14 11:00:13 +0300

    Reduce to Analog value according the key of direction

--- main.cpp
+++ main.cpp
@@ -3,25 +3,74 @@
 #include <thread>
 #include <chrono>
 
-const int pressFrequency = 10;
+const int pressFrequency = 50;
+const int keyArrowUp = 72;
+const int keyArrowDown = 80;
+const int keyArrowRight = 77;
+const int keyArrowLeft = 75;
 
 
 bool getInput(char *c);
 
 int main(void) {
     char key = ' ';
-    int defaultTimeConstantWindows = 300;
+    int analogValue = 255;
 
     while (key != 'q') {
-            std::this_thread::sleep_for(
-                    std::chrono::milliseconds(1000 / pressFrequency));//When we are taking the input sleep x millisecond
-            getInput(&key);
+        std::this_thread::sleep_for(
+                std::chrono::milliseconds(1000 / pressFrequency));//When we are taking the input sleep x millisecond
+        getInput(&key);
 
-            std::cout << "You Pressed: " << key << std::endl;
-            key = ' ';
+        switch (key) {
+            case keyArrowUp:
+                if (analogValue < 255) {
+                    analogValue += 1;
+                    //std::cout << analogValue << std::endl;
+                } else {
+                    analogValue = 255;
+                    //std::cout << analogValue << std::endl;
+                }
+                break;
+
+            case keyArrowDown:
+                if (analogValue > 1) {
+                    analogValue -= 1;
+                    //std::cout << analogValue << std::endl;
+                } else {
+                    analogValue = 0;
+                    //std::cout << analogValue << std::endl;
+                }
+                break;
+
+            case keyArrowRight:
+                if (analogValue < 255) {
+
+                    analogValue += 10;
+                    //std::cout << analogValue << std::endl;
+                } else {
+                    analogValue = 255;
+                    //std::cout << analogValue << std::endl;
+                }
+                break;
+
+            case keyArrowLeft:
+                if (analogValue >= 10) {
+
+                    analogValue -= 10;
+                    //std::cout << analogValue << std::endl;
+                } else {
+                    analogValue = 0;
+                    //std::cout << analogValue << std::endl;
+                }
+                break;
+        }
+
+        std::cout << "Current VAlue: " << analogValue << std::endl;
+        key = ' ';
     }
 
     std::cout << "You Pressed q, End Of Program" << std::endl;
+
     return 0;
 }
 
